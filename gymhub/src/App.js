137f391a279cc6090985e1558ca56806d5d932c3
Brayan Gutierrez Dinarte <38222878+BrayanD17@users.noreply.github.com>
2024-07-24 19:01:19 -0600

    Login
    primer boseto de login

---
+++ gymhub/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import LoginComponent from './components/LoginComponent';
+function App() {
+  return (
+    <div className="App">
+      <LoginComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ gymhub/src/components/LoginComponent.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import '../css/LoginComponent.css';
+import LogoGymHub from '../assets/LogoGymHub.png'; // Asegúrate de tener la ruta correcta al logo
+
+const LoginComponent = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Lógica de inicio de sesión aquí
+    console.log('Login', { username, password });
+  };
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    // Lógica de registro aquí
+    console.log('Register', { username, password });
+  };
+
+  const handleForgotPassword = (e) => {
+    e.preventDefault();
+    // Lógica para manejar la recuperación de contraseña
+    console.log('Forgot Password');
+  };
+
+  return (
+    <div className="login-container">
+      <img src={LogoGymHub} alt="Gym Hub Logo" className="login-logo" />
+      <form>
+        <div className="login-form-group">
+          <label htmlFor="username" className="login-label">Usuario</label>
+          <input 
+            type="text" 
+            id="username" 
+            value={username} 
+            onChange={(e) => setUsername(e.target.value)} 
+            className="login-input"
+          />
+        </div>
+        <div className="login-form-group">
+          <label htmlFor="password" className="login-label">Contraseña</label>
+          <input 
+            type="password" 
+            id="password" 
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)} 
+            className="login-input"
+          />
+        </div>
+        <div className="login-form-group">
+          <button onClick={handleForgotPassword} className="forgot-password-button">¿Olvidaste tu contraseña?</button>
+        </div>
+        <div className="login-button-group">
+          <button onClick={handleLogin} className="login-button login-button-login">Inicio sesión</button>
+          <button onClick={handleRegister} className="login-button login-button-register">Crear cuenta</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default LoginComponent;
