3f60ca0f91072bef4073b844ee4e9ee499ac7874
Olman Alonso Acuña Quesada <61434202+OlmanAQ@users.noreply.github.com>
2024-08-23 17:45:52 -0600

    login con username

---
+++ gymhub/src/components/cliente/ClientNavBarComponent.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import logo from '../../assets/LogoGymHub.png';
+import { User } from 'lucide-react';
+import '../../css/AdminNavBarComponent.css';
+import appFirebase from '../../firebaseConfig/firebase';
+import { getAuth } from 'firebase/auth';
+
+const auth = getAuth(appFirebase);
+
+const ClientNavBarComponent = () => {
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
+
+  // bottom lpg out
+  const handleLogout = () => {
+    auth.signOut().then(() => {
+        console.log('Sesión cerrada');
+        
+    }).catch((error) => {
+        console.log('Error al cerrar sesión', error);
+    });
+};
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img src={logo} alt="Logo" />
+      </div>
+        <ul className="navbar-menu">
+          <li><a href="#rutinas">Rutinas</a></li>
+          <li><a href="#alertas">Alertas</a></li>
+          <li><a href="#suplementos">Suplementos</a></li>
+          <li><a href="#ventas">Ventas</a></li>
+          <li><a href="#premiacion">Premiación</a></li>
+        </ul>
+        <div
+          className="navbar-profile"
+          onMouseEnter={() => setDropdownVisible(true)}
+          onMouseLeave={() => setDropdownVisible(false)}
+        >
+          <User className="navbar-icon" />
+          <span className="navbar-username">Mi Perfil</span>
+          {isDropdownVisible && (
+            <ul className="navbar-dropdown">
+              <li><a href="#ver-perfil">Ver Perfil</a></li>
+              <li><a href="home" onClick={handleLogout}>Cerrar Sesión</a></li>
+            </ul>
+          )}
+        </div>
+    </nav>
+  );
+}
+
+export default ClientNavBarComponent;
--- gymhub/src/components/registro-login/LoginComponent.jsx
+++ gymhub/src/components/registro-login/LoginComponent.jsx
@@ -2,11 +2,12 @@ import React, { useState } from 'react';
 import '../../css/LoginComponent.css';
 import LogoGymHub from '../../assets/LogoGymHub.png';
 import Swal from 'sweetalert2';
+import { collection, query, where, getDocs} from 'firebase/firestore';
 
 
-import appFirebase from '../../firebaseConfig/firebase';
-import { getAuth,signInWithEmailAndPassword } from 'firebase/auth';
-const auth = getAuth(appFirebase);
+
+import {db, auth} from '../../firebaseConfig/firebase';
+import {signInWithEmailAndPassword } from 'firebase/auth';
 
 const LoginComponent = ({ onShowRegister }) => {
   const [username, setUsername] = useState('');
@@ -16,26 +17,60 @@ const LoginComponent = ({ onShowRegister }) => {
   const handleLogin = async(e) => {
     e.preventDefault();
 
-    signInWithEmailAndPassword(auth, username, password)
-      .then((userCredential) => {
-        
-        const user = userCredential.user;
-        console.log('Inicio de sesión exitoso', user);
+    // Verificar que los campos no estén vacíos
+    if (username === '' || password === '') {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'Por favor, llena todos los campos',
+      });
+      return;
+    }
 
+    // Si el username es correo iniciar sesión con correo y contraseña, buscar el correo en la base de datos si no existe mostrar mensaje de error
+
+    if (username.includes('@')) {
+      try {
+        const user = await signInWithEmailAndPassword(auth, username, password);
+        console.log(user);
+        // Redirigir a la página de inicio
       }
-
-      )
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log('Error al iniciar sesión', errorCode, errorMessage);
+      catch (error) {
         Swal.fire({
           icon: 'error',
           title: 'Error',
           text: 'Usuario o contraseña incorrectos',
         });
       }
-      );
+    }
+
+    // Si el username no es correo buscar el username en el firebase y obtener el correo para iniciar sesión
+    else {
+      try {
+        const q = query(collection(db, 'User'), where('usuario', '==', username));
+        const querySnapshot = await getDocs(q);
+        if (!querySnapshot.empty) {
+          querySnapshot.forEach((doc) => {
+            const user = doc.data();
+            signInWithEmailAndPassword(auth, user.correo, password);
+            console.log(user);
+          });
+        } else {
+          Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Usuario o contraseña incorrectos',
+          });
+        }
+      } catch (error) {
+        console.error('Error al buscar el usuario: ', error);
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'Ocurrió un error al iniciar sesión',
+        });
+      }
+    }
   };
 
   const handleForgotPassword = (e) => {
